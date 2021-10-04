93e690dc98760bd0d646d80f94e094f7f3d0fcc6
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2021-10-04 15:08:17 -0500

    Add basic scaffolding to support (s)css and typescript (with jsx)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
+  <Test></Test>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import {Test} from './components/Test'
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Test,
   }
-}
+});
 </script>
 
 <style>
