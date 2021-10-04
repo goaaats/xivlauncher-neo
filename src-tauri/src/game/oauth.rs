ae87df28a2f08dc7761872b0b1241e6f0cc2ea3e
goat <16760685+goaaats@users.noreply.github.com>
2021-10-04 16:54:14 +0200

    Initial commit

---
+++ src-tauri/src/game/oauth.rs
@@ -0,0 +1,9 @@
+enum OauthLoginResult {
+
+}
+
+impl Oauth {
+    pub fn login(username: &str, password: &str, otp: &str) -> OauthLoginResult {
+
+    }
+}
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,41 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use log::LevelFilter;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::append::file::FileAppender;
+use log4rs::encode::pattern::PatternEncoder;
+use log4rs::Config;
+use log4rs::config::{Appender, Logger, Root};
+
+fn main() {
+  setup_log();
+
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
+fn setup_log() {
+  let stdout = ConsoleAppender::builder().build();
+
+  let requests = FileAppender::builder()
+      .encoder(Box::new(PatternEncoder::new("{d} - {m}{n}")))
+      .build("log/requests.log")
+      .unwrap();
+
+  let config = Config::builder()
+      .appender(Appender::builder().build("stdout", Box::new(stdout)))
+      .appender(Appender::builder().build("requests", Box::new(requests)))
+      .logger(Logger::builder().build("app::backend::db", LevelFilter::Info))
+      .logger(Logger::builder()
+          .appender("requests")
+          .additive(false)
+          .build("app::requests", LevelFilter::Info))
+      .build(Root::builder().appender("stdout").build(LevelFilter::Warn))
+      .unwrap();
+
+  log4rs::init_config(config).unwrap();
+}
