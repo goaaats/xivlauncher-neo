f0eb49d1cc7e7d39f0e1438e2a8962743249a183
goat <16760685+goaaats@users.noreply.github.com>
2021-10-05 15:44:27 +0200

    add launcher constants

--- src-libxl/src/game/constants.rs
+++ src-libxl/src/game/constants.rs
@@ -1,2 +1,21 @@
-pub static FRONTIER_GATE_STATUS: &str = "https://frontier.ffxiv.com/worldStatus/gate_status.json";
-pub static FRONTIER_LOGIN_STATUS: &str = "https://frontier.ffxiv.com/worldStatus/login_status.json";
+pub static FRONTIER_GATE_STATUS_URL: &str = "https://frontier.ffxiv.com/worldStatus/gate_status.json";
+pub static FRONTIER_LOGIN_STATUS_URL: &str = "https://frontier.ffxiv.com/worldStatus/login_status.json";
+
+pub static PATCH_BOOTVER_URL: &str = "http://patch-bootver.ffxiv.com/http/win32/ffxivneo_release_boot/{}/?time={}";
+pub static PATCH_GAMEVER_URL: &str = "https://patch-gamever.ffxiv.com/http/win32/ffxivneo_release_game/{}/{}";
+
+pub static OAUTH_TOP_URL: &str = "https://ffxiv-login.square-enix.com/oauth/ffxivarr/login/top?lng=en&rgn={}&isft=0&cssmode=1&isnew=1&issteam={}";
+pub static OAUTH_SEND_URL: &str = "https://ffxiv-login.square-enix.com/oauth/ffxivarr/login/login.send";
+
+pub static USER_AGENT_TEMPLATE: &str = "SQEXAuthor/2.0.0(Windows 6.2; ja-jp; {0})";
+
+pub static STEAM_APP_ID: i32 = 39210;
+
+pub static PATCH_GAMEVER_HASHES: [&'static str; 6] = [
+    "ffxivboot.exe",
+    "ffxivboot64.exe",
+    "ffxivlauncher.exe",
+    "ffxivlauncher64.exe",
+    "ffxivupdater.exe",
+    "ffxivupdater64.exe",
+];
--- src-libxl/src/game/status.rs
+++ src-libxl/src/game/status.rs
@@ -1,4 +1,4 @@
-use crate::game::constants::{FRONTIER_GATE_STATUS, FRONTIER_LOGIN_STATUS};
+use crate::game::constants::{FRONTIER_GATE_STATUS_URL, FRONTIER_LOGIN_STATUS_URL};
 
 use serde::Deserialize;
 
@@ -15,9 +15,9 @@ async fn get_status(url: &str) -> Result<bool, reqwest::Error> {
 }
 
 pub async fn is_gate() -> Result<bool, reqwest::Error> {
-    get_status(FRONTIER_GATE_STATUS).await
+    get_status(FRONTIER_GATE_STATUS_URL).await
 }
 
 pub async fn is_login() -> Result<bool, reqwest::Error> {
-    get_status(FRONTIER_LOGIN_STATUS).await
+    get_status(FRONTIER_LOGIN_STATUS_URL).await
 }
