692bae1aae198abac234be1ecb23f568d54f8439
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-10-06 13:28:59 -0700

    Remove unnecessary IDs on title bar elements

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -10,19 +10,19 @@ const TitleBar = defineComponent({
         return <div data-tauri-drag-region class={styles.titlebar}>
             <span class={styles.titlebarText}>{this.title}</span>
             <div>
-                <div class={styles.titlebarButton} id="titlebar-minimize" onClick={() => appWindow.minimize()}>
+                <div class={styles.titlebarButton} onClick={() => appWindow.minimize()}>
                     <img
                     src="https://api.iconify.design/mdi:window-minimize.svg"
                     alt="minimize"
                     />
                 </div>
-                <div class={styles.titlebarButton} id="titlebar-maximize" onClick={() => appWindow.toggleMaximize()}>
+                <div class={styles.titlebarButton} onClick={() => appWindow.toggleMaximize()}>
                     <img
                     src="https://api.iconify.design/mdi:window-maximize.svg"
                     alt="maximize"
                     />
                 </div>
-                <div class={styles.titlebarButtonClose} id="titlebar-close" onClick={() => appWindow.close()}>
+                <div class={styles.titlebarButtonClose} onClick={() => appWindow.close()}>
                     <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
                 </div>
             </div>
