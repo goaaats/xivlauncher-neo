5400657ba55e3935f2022222726fd090f5e6987d
goaaats <16760685+goaaats@users.noreply.github.com>
2021-11-07 16:59:40 +0100

    oauth wip

--- libxl/src/game/oauth.rs
+++ libxl/src/game/oauth.rs
@@ -1,13 +1,17 @@
 use core::fmt;
+use std::collections::HashMap;
 use lazy_static::lazy_static;
 use regex::Regex;
+use reqwest::multipart;
 
 use crate::game::{constants, request, platform::Platform, ClientLanguage};
 
 #[derive(Debug)]
 pub enum LoginError {
     NoStored,
+    NoResultMatch,
 
+    Account(String),
     Reqwest(reqwest::Error),
 }
 
@@ -42,15 +46,41 @@ pub async fn login(
     region: AccountRegion,
 ) -> Result<LoginResult, LoginError> {
     let stored = stored(steam_service, region).await?;
-    println!("{}", stored);
 
-    Ok(LoginResult {
-        session_id: "0".to_string(),
-        can_play: true,
-        terms_accepted: true,
-        entitled_expansion: 4,
-        region: AccountRegion::Japan,
-    })
+    let mut form = HashMap::new();
+    form.insert("_STORED_", stored);
+    form.insert("sqexid", username.to_string());
+    form.insert("password", password.to_string());
+    form.insert("otppw", otp.to_string());
+
+    let resp = request::launcher_get(constants::OAUTH_SEND_URL)
+        .header("Cache-Control", "no-cache")
+        .header("Accept", "image/gif, image/jpeg, image/pjpeg, application/x-ms-application, application/xaml+xml, application/x-ms-xbap, */*")
+        .header("Accept-Encoding", "gzip, deflate")
+        .header("Accept-Language", "en-US,en;q=0.9")
+        .header("Referer", request::launcher_referer(ClientLanguage::English))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .form(&form)
+        .send().await.map_err(LoginError::Reqwest)?;
+
+    let text = resp.text().await.map_err(LoginError::Reqwest)?;
+
+    lazy_static! {
+        static ref RE_LOGINRES: Regex =
+            Regex::new(r#"(?m)<\s*input .* name="_STORED_" value="(.*)">"#).unwrap();
+    }
+    
+    if let Some(login_captures) = RE_LOGINRES.captures_iter(&text).next() {
+        let params = login_captures[1]
+
+        return Ok(login_captures[1].to_string());
+    }
+    
+    Err(LoginError::NoResultMatch)
+}
+
+fn get_oauth_referer(language: ClientLanguage, region: AccountRegion, steam_service: bool) -> String {
+    
 }
 
 async fn stored(steam_service: bool, region: AccountRegion) -> Result<String, LoginError> {
