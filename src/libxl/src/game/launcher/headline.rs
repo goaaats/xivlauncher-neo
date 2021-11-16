fbf808124e4efa80e418f0e08fefa71bab7ac5d8
Raymond <daemitus@gmail.com>
2021-11-16 13:23:47 -0500

    Headlines need to be serializable to pass through tauri

--- src/libxl/src/game/launcher/headline.rs
+++ src/libxl/src/game/launcher/headline.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, Utc};
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 
 use crate::{game::{ClientLanguage, request}, util::time};
 
@@ -8,23 +8,22 @@ pub enum HeadlineError {
     Reqwest(reqwest::Error),
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct HeadlineEntry {
     pub date: DateTime<Utc>,
     pub title: String,
     pub url: String,
     pub id: String,
-
     pub tag: Option<String>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct BannerEntry {
     pub lsb_banner: String,
     pub link: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Headline {
     pub news: Vec<HeadlineEntry>,
     pub topics: Vec<HeadlineEntry>,
@@ -35,7 +34,6 @@ pub struct Headline {
 impl Headline {
     pub async fn get(language: ClientLanguage) -> Result<Self, HeadlineError> {
         let url = format!("https://frontier.ffxiv.com/news/headline.json?lang={}&media=pcapp&{}", language.langcode(), time::utc_now_millis());
-
         let resp = request::launcher_get(&url).send().await.map_err(HeadlineError::Reqwest)?;
         Ok(resp.json().await.map_err(HeadlineError::Reqwest)?)
     }
