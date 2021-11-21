80c12ca4c0f5fe17b48587ccf630df1b25c31fda
goaaats <16760685+goaaats@users.noreply.github.com>
2021-11-21 03:26:34 +0100

    feat: integrity progress

--- src/libxl/src/game/integrity.rs
+++ src/libxl/src/game/integrity.rs
@@ -6,6 +6,11 @@ use std::{
   fs::File,
   io::BufReader,
   path::{Path, PathBuf},
+  sync::{
+    atomic::AtomicU32,
+    mpsc::{Receiver, Sender},
+    Arc, Mutex,
+  },
 };
 use walkdir::WalkDir;
 
@@ -21,21 +26,60 @@ pub struct IntegrityCheckModel {
 }
 
 impl IntegrityCheckModel {
-  pub fn generate(version: &str, game_path: &Path) -> IntegrityCheckModel {
+  pub fn check(&self, game_path: &Path, progress: Arc<AtomicU32>) -> Integrity {
+    println!("Reference version: {:#?}", self.version);
+
+    let mut report = IntegrityCheckModel::generate(&self.version, game_path, progress, Some(self));
+    let mut failed: Vec<String> = Vec::new();
+
+    let mut has_missing = false;
+    for (k, v) in report.hashes.iter_mut() {
+      if self.hashes.get(k).is_none() {
+        failed.push(k.clone());
+        has_missing = true;
+      } else if v != self.hashes.get(k).unwrap() {
+        failed.push(k.clone());
+      }
+    }
+
+    if report.hashes.is_empty() {
+      has_missing = true;
+    }
+
+    Integrity {
+      version: self.version.clone(),
+      failed_files: if !failed.is_empty() { Some(failed) } else { None },
+      game_path: game_path.to_path_buf(),
+      missing_files: has_missing
+    }
+  }
+
+  pub fn generate(version: &str, game_path: &Path, progress: Arc<AtomicU32>, reference: Option<&IntegrityCheckModel>) -> IntegrityCheckModel {
     let paths: Vec<PathBuf> = WalkDir::new(game_path)
       .into_iter()
       .filter_map(|v| v.ok())
+      // We only want files
       .filter(|v| v.path().is_file())
+      // If we have a reference, we only want files that are in the reference
+      .filter(|v| if let Some(reference) = reference {
+        reference.hashes.contains_key(v.path().to_str().unwrap())
+      } else {
+        true
+      })
       .map(|x| x.path().to_path_buf())
       .collect();
 
     let hashes: HashMap<String, String> = paths
       .par_iter()
       .map(|x| {
-        (
+        let result = (
           x.to_str().unwrap().to_string(),
           IntegrityCheckModel::make_hash(x).expect("Could not generate hash."),
-        )
+        );
+
+        progress.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+
+        result
       })
       .collect();
 
@@ -53,39 +97,21 @@ impl IntegrityCheckModel {
   }
 }
 
+#[derive(Debug)]
 pub struct Integrity {
   pub version: String,
-  pub failed: Option<Vec<String>>,
+  pub failed_files: Option<Vec<String>>,
   pub game_path: PathBuf,
+  pub missing_files: bool,
 }
 
 impl Integrity {
-  pub async fn check(version: String, game_path: &Path) -> Integrity {
-    let reference = reqwest::get(format!("{}/{}.json", INTEGRITY_REPO_URL, version))
-      .await
-      .expect("Failed to get integrity report")
-      .json::<IntegrityCheckModel>()
-      .await
-      .expect("Failed to parse integrity report");
-
-    println!("Reference version: {:#?}", reference);
-
-    let mut report = IntegrityCheckModel::generate(&version, game_path);
-    let mut failed: Vec<String> = Vec::new();
-
-    for (k, v) in report.hashes.iter_mut() {
-      if reference.hashes.get(k).is_none() {
-        failed.push(k.clone());
-        continue;
-      } else if v != reference.hashes.get(k).unwrap() {
-        failed.push(k.clone());
-      }
-    }
-
-    Integrity {
-      version,
-      failed: if !failed.is_empty() { Some(failed) } else { None },
-      game_path: game_path.to_path_buf(),
-    }
+  pub async fn get_reference(version: String) -> Result<IntegrityCheckModel, reqwest::Error> {
+    Ok(
+      reqwest::get(format!("{}/{}.json", INTEGRITY_REPO_URL, version))
+        .await?
+        .json::<IntegrityCheckModel>()
+        .await?
+    )
   }
 }
--- src/libxl/tests/integrity.rs
+++ src/libxl/tests/integrity.rs
@@ -1,8 +1,14 @@
-use libxl::game::{
-  integrity::Integrity,
-  repository::Repository,
+use libxl::game::{integrity::{Integrity, IntegrityCheckModel}, repository::Repository};
+use std::{
+  path::{Path, PathBuf},
+  sync::{
+    atomic::AtomicU32,
+    mpsc::{self, TryRecvError},
+    Arc,
+  },
+  thread,
+  time::Duration,
 };
-use std::path::{Path, PathBuf};
 
 fn get_game_path() -> PathBuf {
   let game_path = dbg!(env!("XL_TESTS_GAMEPATH"));
@@ -11,24 +17,61 @@ fn get_game_path() -> PathBuf {
 
 #[test]
 fn gen_integrity() {
+  let game_path = get_game_path();
+  let version = Repository::FFXIV.get_version(&game_path);
+
+  let progress = Arc::new(AtomicU32::default());
+  let report = IntegrityCheckModel::generate(&version, &game_path, progress, None);
+
+  println!("{:#?}", report);
+}
+
+#[test]
+fn check_integrity() {
   macro_rules! aw {
     ($e:expr) => {
       tokio_test::block_on($e)
     };
   }
 
+  let start = std::time::Instant::now();
+
   let game_path = get_game_path();
 
   let ver = Repository::FFXIV.get_version(&game_path);
-  let model = aw!(Integrity::check(ver, &game_path));
-  return;
+  let reference = aw!(Integrity::get_reference(ver)).expect("Could not get reference report.");
+  let num_hashes = reference.hashes.len();
 
-  match model.failed {
-    Some(failed) => {
-      println!("failed: {:?}", failed);
+  let progress = Arc::new(AtomicU32::default());
+
+  let (tx, rx) = mpsc::channel();
+
+  let thread_progress = progress.clone();
+
+  thread::spawn(move || loop {
+    let prog = thread_progress.load(std::sync::atomic::Ordering::Relaxed);
+    println!(
+      "{:08}/{:08} ({:.2}%)",
+      prog,
+      num_hashes,
+      prog as f32 / num_hashes as f32 * 100.0
+    );
+
+    thread::sleep(Duration::from_millis(500));
+    match rx.try_recv() {
+      Ok(_) | Err(TryRecvError::Disconnected) => {
+        println!("Terminating.");
+        break;
+      }
+      Err(TryRecvError::Empty) => {}
     }
-    None => {
-      println!("success");
-    }
-  }
+  });
+
+  let report = reference.check(&game_path, progress);
+
+  let _ = tx.send(());
+
+  let elapsed = start.elapsed();
+  println!("Hashes calculated in: {:.2?}", elapsed);
+  println!("{:?}", report);
 }
