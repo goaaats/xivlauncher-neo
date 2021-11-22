96e2f3dee033e2eaa067585b2f3a3979fac4ffa0
goat <16760685+goaaats@users.noreply.github.com>
2021-11-22 04:55:32 +0100

    region: implement From<u8>

--- src/libxl/src/game/region.rs
+++ src/libxl/src/game/region.rs
@@ -1,15 +1,21 @@
 use strum_macros::EnumString;
 
-#[derive(Copy, Clone, EnumString)]
+#[derive(Copy, Clone, Debug, EnumString)]
 pub enum AccountRegion {
   Japan = 1,
   NorthAmerica = 2,
   Europe = 3,
 }
 
+// TODO(goat): Is it possible to macro this?
 impl From<u8> for AccountRegion {
     fn from(val: u8) -> Self {
-        todo!()
+        match val {
+            1 => AccountRegion::Japan,
+            2 => AccountRegion::NorthAmerica,
+            3 => AccountRegion::Europe,
+            _ => panic!("Unimplemented region")
+        }
     }
 }
 
